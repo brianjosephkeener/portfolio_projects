930358baf059e47f167b64351b740fa512e54ed1
Brian Keener <brian120496@gmail.com>
2022-02-06 03:13:40 -0500

    icon updates

--- keener_coding/client/src/views/Index.js
+++ keener_coding/client/src/views/Index.js
@@ -6,17 +6,25 @@ const Index = (props) => {
       <div className="zindex top-page">
         <h1 className='text-center w'>Hey, I'm <span className='text-primary'>Brian</span>.</h1>
         <h1 className='text-center w'>I'm a fullstack web developer.</h1>
-        <button className='btn btn-danger center-me'>View my Work</button>
+        <button className='btn btn-danger center-me mt'><a href='https://github.com/Skull86' target={"_blank"}>View my Work</a></button>
       </div>
-      <div>
-      <img src='images\c-logo.png' alt='C# logo'></img>
-      <img src='images\mongodb-logo.png' alt='mongodb logo'></img>
-      <img src='images\mysql-logo.png' alt='mysql logo'></img>
-      <img src='images\python-logo.png' alt='python logo'></img>
-      <img src='images\react-png.png' alt='react logo'></img>
-      <img src='images\javascript-logo.png' alt='javascript logo'></img>
-      <img src='images\html-logo.png' alt='html logo'></img>
-      <img src='images\css-logo.png' alt='css logo'></img>
+      <div className='logo-container mt'>
+        <div className='logo-row mt'>
+          <img src='images\c-logo.png' alt='C# logo'></img>
+          <img src='images\python-logo.png' alt='python logo'></img>
+          <img src='images\flask.jpg' alt='react logo'></img>
+          <img src='images\react-png.png' alt='react logo'></img>
+          
+          </div>
+        <div className='logo-row mt'>
+          <img src='images\mongodb-logo.png' alt='mongodb logo' id='mongoDB'></img>
+          <img src='images\mysql-logo.png' alt='mysql logo'></img>
+          </div>
+        <div className='logo-row mt'>
+        <img src='images\javascript-logo.png' alt='javascript logo'></img>
+          <img src='images\html-logo.png' alt='html logo'></img>
+          <img src='images\css-logo.png' alt='css logo'></img>
+        </div>
       </div>
     </div>
   );

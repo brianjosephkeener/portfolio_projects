5eb8cdbd1895566ca63c8115e5d13087058e6255
Brian Keener <brian120496@gmail.com>
2022-02-14 23:22:59 -0500

    hamburger retract & extend testing

--- canoo_clone_python/static/script.js
+++ canoo_clone_python/static/script.js
@@ -1,1 +1,18 @@
-console.log("test");
+let hampos = 0;
+const lines = document.querySelectorAll("line");
+
+function test() {
+    console.log("test retract")
+    const hammenu = document.getElementById("hamburger");
+    hammenu.style.display = "flex";
+    lines.forEach(element => {
+        element.createElement('div');
+        element.classList = "line";
+    });
+}
+
+function extendnav() {
+    console.log("test extend")
+    const hammenu = document.getElementById("hamburger");
+    hammenu.style.display = "none";
+  }

c4736791dbcac4a5f8d1e02eedbbca378268a9ba
Brian Keener <brian120496@gmail.com>
2022-02-17 00:15:07 -0500

    logic done need to fix styling

--- canoo_clone_python/static/script.js
+++ canoo_clone_python/static/script.js
@@ -1,7 +1,5 @@
 let view = 0;
 
-const lines = document.querySelectorAll("line");
-
 function retractnav() {
     const hammenu = document.getElementById("hamburger");
     hammenu.style.display = "flex";
@@ -26,12 +24,11 @@ function extendnav() {
         actionmenu.style.left = "0";
         actionmenu.style.zIndex = "10";
     },750);
-    console.log(extendmenu);
     extendmenu.id = "extmenu";
   }
 
   let currslide = 1;
-
+  const lines = document.getElementsByName("line");
   function carouselmov(movamount) {
       if(movamount == 1 && currslide == 3)
       {
@@ -42,8 +39,65 @@ function extendnav() {
           currslide = 3;
       }
       else {
-        currslide++;
+        if(movamount == 1)
+        {
+            currslide++;
+        }
+        else if(movamount == -1)
+        {
+            currslide--;
+        }
       }
       const currimage = document.getElementById("first-top-img");
-      currimage.src =`static/home-${currslide}-canoo.png`
+      const lines = document.getElementsByName("line");
+      const fp_text = document.getElementsByName("fp-text");
+      currimage.src =`static/home-${currslide}-canoo.png`;
+      switch (currslide) {
+        case 1:
+            fp_text[0].innerHTML = "LIFESTYLE VEHICLE";
+            fp_text[1].innerHTML = "A LOFT ON WHEELS";
+            fp_text[2].innerHTML = "Launching late 2022. U.S. Starting at $34,750";
+            lines[0].className = "hp-line-sel";
+            lines[1].className = "hp-line-unsel";
+            lines[2].className = "hp-line-unsel";
+              break;
+        case 2:
+            fp_text[0].innerHTML = "PICKUP";
+            fp_text[1].innerHTML = "A PURPOSE-BUILT PICKUP TRUCK";
+            fp_text[2].innerHTML = "Launching as early as 2023. U.S. Preorders Now Open";
+            lines[1].className = "hp-line-sel";
+            lines[0].className = "hp-line-unsel";
+            lines[2].className = "hp-line-unsel";
+            break;
+        case 3:
+            fp_text[0].innerHTML = "MPDV";
+            fp_text[1].innerHTML = "MULTI-PURPOSE DELIVERY VEHICLE";
+            fp_text[2].innerHTML = "";
+            lines[2].className = "hp-line-sel";
+            lines[1].className = "hp-line-unsel";
+            lines[0].className = "hp-line-unsel";
+      }
   }
+
+  function carouselmovline(slidenumber) {
+      currslide = slidenumber;
+    const currimage = document.getElementById("first-top-img");
+    const lines = document.getElementsByName("line");
+    currimage.src =`static/home-${currslide}-canoo.png`;
+    switch (currslide) {
+      case 1:
+          lines[0].className = "hp-line-sel";
+          lines[1].className = "hp-line-unsel";
+          lines[2].className = "hp-line-unsel";
+            break;
+      case 2:
+          lines[1].className = "hp-line-sel";
+          lines[0].className = "hp-line-unsel";
+          lines[2].className = "hp-line-unsel";
+          break;
+      case 3:
+          lines[2].className = "hp-line-sel";
+          lines[1].className = "hp-line-unsel";
+          lines[0].className = "hp-line-unsel";
+    }
+}

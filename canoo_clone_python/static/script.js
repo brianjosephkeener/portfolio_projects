dfdefc1ac40cefd7fa52f99a1edf6651222c66cb
Brian Keener <brian120496@gmail.com>
2022-02-17 11:42:30 -0500

    margin changes

--- canoo_clone_python/static/script.js
+++ canoo_clone_python/static/script.js
@@ -57,6 +57,7 @@ function extendnav() {
             fp_text[0].innerHTML = "LIFESTYLE VEHICLE";
             fp_text[1].innerHTML = "A LOFT ON WHEELS";
             fp_text[2].innerHTML = "Launching late 2022. U.S. Starting at $34,750";
+            fp_text[1].style.marginTop = "0px";
             lines[0].className = "hp-line-sel";
             lines[1].className = "hp-line-unsel";
             lines[2].className = "hp-line-unsel";
@@ -65,6 +66,7 @@ function extendnav() {
             fp_text[0].innerHTML = "PICKUP";
             fp_text[1].innerHTML = "A PURPOSE-BUILT PICKUP TRUCK";
             fp_text[2].innerHTML = "Launching as early as 2023. U.S. Preorders Now Open";
+            fp_text[1].style.marginTop = "0px";
             lines[1].className = "hp-line-sel";
             lines[0].className = "hp-line-unsel";
             lines[2].className = "hp-line-unsel";
@@ -72,6 +74,7 @@ function extendnav() {
         case 3:
             fp_text[0].innerHTML = "MPDV";
             fp_text[1].innerHTML = "MULTI-PURPOSE DELIVERY VEHICLE";
+            fp_text[1].style.marginTop = "-20px";
             fp_text[2].innerHTML = "";
             lines[2].className = "hp-line-sel";
             lines[1].className = "hp-line-unsel";
@@ -83,21 +86,34 @@ function extendnav() {
       currslide = slidenumber;
     const currimage = document.getElementById("first-top-img");
     const lines = document.getElementsByName("line");
+    const fp_text = document.getElementsByName("fp-text");
     currimage.src =`static/home-${currslide}-canoo.png`;
     switch (currslide) {
-      case 1:
-          lines[0].className = "hp-line-sel";
-          lines[1].className = "hp-line-unsel";
-          lines[2].className = "hp-line-unsel";
+        case 1:
+            fp_text[0].innerHTML = "LIFESTYLE VEHICLE";
+            fp_text[1].innerHTML = "A LOFT ON WHEELS";
+            fp_text[2].innerHTML = "Launching late 2022. U.S. Starting at $34,750";
+            fp_text[1].style.marginTop = "0px";
+            lines[0].className = "hp-line-sel";
+            lines[1].className = "hp-line-unsel";
+            lines[2].className = "hp-line-unsel";
+              break;
+        case 2:
+            fp_text[0].innerHTML = "PICKUP";
+            fp_text[1].innerHTML = "A PURPOSE-BUILT PICKUP TRUCK";
+            fp_text[2].innerHTML = "Launching as early as 2023. U.S. Preorders Now Open";
+            fp_text[1].style.marginTop = "0px";
+            lines[1].className = "hp-line-sel";
+            lines[0].className = "hp-line-unsel";
+            lines[2].className = "hp-line-unsel";
             break;
-      case 2:
-          lines[1].className = "hp-line-sel";
-          lines[0].className = "hp-line-unsel";
-          lines[2].className = "hp-line-unsel";
-          break;
-      case 3:
-          lines[2].className = "hp-line-sel";
-          lines[1].className = "hp-line-unsel";
-          lines[0].className = "hp-line-unsel";
-    }
+        case 3:
+            fp_text[0].innerHTML = "MPDV";
+            fp_text[1].innerHTML = "MULTI-PURPOSE DELIVERY VEHICLE";
+            fp_text[1].style.marginTop = "-20px";
+            fp_text[2].innerHTML = "";
+            lines[2].className = "hp-line-sel";
+            lines[1].className = "hp-line-unsel";
+            lines[0].className = "hp-line-unsel";
+      }
 }

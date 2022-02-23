7687bd7bf3d086f2ec6be24eb9bf75ac159b5da8
Brian Keener <brian120496@gmail.com>
2022-02-23 15:45:28 -0500

    flask-mail set up for lead generator

--- canoo_clone_python/server.py
+++ canoo_clone_python/server.py
@@ -1,15 +1,32 @@
-from crypt import methods
 from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
 app = Flask(__name__)
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USERNAME'] = None
+app.config['MAIL_PASSWORD'] = '' #change to fit sender
+app.config['MAIL_DEFAULT_SENDER'] = 'brianjosephkeener@gmail.com'
+app.config['MAIL_MAX_EMAIL'] = None
+app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['MAIL_ASCII_ATTACHMENTS'] = False
+
+
+mail = Mail(app)
+
 @app.route('/')          
-def home_page():
+def index():
     return render_template('index.html')
 
 @app.route('/leadgen', methods=['POST'])
 def create_email():
-    print("TEST RECEIVED FORM")
-    print(request.form['fname'])
+    print("RECEIVED FORM")
+    subOfI = request.form['email']
+    msg = Message("You signed up for the Fake Canoo Newsletter!", recipients=[f'{subOfI}'])
+    mail.send(msg)
     return redirect('/')
 
 

ca83784390151bebca30b8a150983f15aa80645a
Brian Keener <brian120496@gmail.com>
2022-03-22 10:49:27 -0400

    save

---
+++ Speech_Recogition_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace Speech_Recogition_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        byte counter = 0;
+        Random ran = new Random();
+        SpeechRecognitionEngine recognize = new SpeechRecognitionEngine();
+        SpeechSynthesizer syn = new SpeechSynthesizer();
+        SpeechRecognitionEngine listen = new SpeechRecognitionEngine();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            recognize.SetInputToDefaultAudioDevice();
+            recognize.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"commands.txt")))));
+            recognize.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default);
+            recognize.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(RecognizeSD);
+            recognize.RecognizeAsync(RecognizeMode.Multiple);
+
+            listen.SetInputToDefaultAudioDevice();
+            listen.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"commands.txt")))));
+            listen.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(ListenSR);
+
+        }
+
+        private void Default(object sender, SpeechRecognizedEventArgs e)
+        {
+            int randomNum;
+            string speech = e.Result.Text;
+
+            if(speech == "Calculator")
+            {
+                syn.SpeakAsync("Yes? what now?");
+            }
+        }
+        private void RecognizeSD(object sender, SpeechDetectedEventArgs e)
+        {
+            counter = 0;
+        }
+        private void ListenSR(object sender, SpeechRecognizedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
